9f502990aed468b0ceb7667eacf6cbae0231d4ea
FatimeyiZahra <fatimeyizahrai@gmail.com>
2025-08-15 12:49:33 +0400

    multi step form _ react hook form

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from '@/components/ui/provider';
-import App from './App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "@/components/ui/provider";
+// import App from "./App.tsx";
+import { OnboardingForm } from "./pages/onborarding-form/onboarding-form.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      {/* <App /> */}
+      <OnboardingForm />
     </Provider>
   </StrictMode>
 );
